e650026ed0c8502dd8a7f4e86e23c875e8b7cc35
adarsh-vishwakarma <adarshvishwakarma704@gmail.com>
2024-10-09 07:08:24 +0530

    get all user data

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -87,25 +87,49 @@ router.post("/signin", async (req, res) => {
 });
 
 const updateBody = zod.object({
-    password: zod.string().optional(),
-    firstName: zod.string().optional(),
-    lastName: zod.string().optional()
-})
+  password: zod.string().optional(),
+  firstName: zod.string().optional(),
+  lastName: zod.string().optional(),
+});
 
-router.put("/", authMiddleware, async(req, res) => {
-    const parsed = updateBody.safeParse(req.body);
-    if(!parsed.success) {
-        res.status(411).json({
-            message: "Error while updating information"
-        })
-    }
-    const updates = parsed.data; // Extract validated data
-    await User.updateOne({ _id: req.userId }, { $set: updates });
-  
+router.put("/", authMiddleware, async (req, res) => {
+  const parsed = updateBody.safeParse(req.body);
+  if (!parsed.success) {
+    res.status(411).json({
+      message: "Error while updating information",
+    });
+  }
+  const updates = parsed.data; // Extract validated data
+  await User.updateOne({ _id: req.userId }, { $set: updates });
 
-    res.json({
-        message: "Updated successfully"
-    })
-})
+  res.json({
+    message: "Updated successfully",
+  });
+});
+
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+  console.log(filter)
+  const users = await User.find({
+    $or: [{
+      firstName: {
+          "$regex": filter
+      }
+  }, {
+      lastName: {
+          "$regex": filter
+      }
+  }]
+  });
+  console.log(users);
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
 
 module.exports = router;
